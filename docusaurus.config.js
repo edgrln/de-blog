87e7ae87765dd0aa958454c22845d09f73be9606
Edgar Lakshin <edgarlakshin@Edgars-MBP.lan>
2025-03-29 20:49:29 +0100

    4

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Edgar's site",
+  title: "Edgar site",
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
